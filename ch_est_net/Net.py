750b8d487e7a60ad7a842cba160011c0cae9760a
Andrew <andsmi97@yandex.ru>
2021-05-26 23:57:48 +0300

    added converting to beam domain and params to NET for working in beam domain

--- ch_est_net/Net.py
+++ ch_est_net/Net.py
@@ -7,47 +7,50 @@ from ch_est_net.utils import DFT_matrixes, MM
 
 
 class Net_Layer(torch.nn.Module):
-    
-    def __init__(self, cfg, freq_samples, time_samples, Trainable = True, isPass = False):
-        super(Net_Layer,self).__init__()
 
-        self.S1 = torch.nn.Parameter(torch.tensor([cfg.S1]), requires_grad = False)
-        self.S2 = torch.nn.Parameter(torch.tensor([cfg.S2]), requires_grad = False)
-        
-        self.DFT_re, self.DFT_im, self.IDFT_re, self.IDFT_im = DFT_matrixes(freq_samples, time_samples, True)
+    def __init__(self, cfg, freq_samples, time_samples, Trainable=True, isPass=False):
+        super(Net_Layer, self).__init__()
+
+        self.S1 = torch.nn.Parameter(
+            torch.tensor([cfg.S1]), requires_grad=False)
+        self.S2 = torch.nn.Parameter(
+            torch.tensor([cfg.S2]), requires_grad=False)
+
+        self.DFT_re, self.DFT_im, self.IDFT_re, self.IDFT_im = DFT_matrixes(
+            freq_samples, time_samples, True)
 
         self.isPass = isPass
         self.cfg = cfg
-
+        self.recived = cfg.recived
 
     def setTrainable(self, value: bool):
-        if value == True: 
+        if value == True:
             self.S1.requires_grad = True
             self.S2.requires_grad = True
-        
+
         else:
             self.S1.requires_grad = False
             self.S1.grad = None
             self.S2.requires_grad = False
             self.S2.grad = None
-    
 
     def forward(self, u_re, u_im, z_re, z_im, H_hat_re, H_hat_im):
         if self.isPass == False:
 
-            Z_re, Z_im = MM(z_re, z_im, self.IDFT_re , self.IDFT_im)
+            Z_re, Z_im = MM(z_re, z_im, self.IDFT_re, self.IDFT_im)
 
             R_re = H_hat_re + Z_re
             R_im = H_hat_im + Z_im
 
-            R_mean = torch.sqrt(torch.mean((R_re**2 + R_im**2), dim = 0))
+            R_mean = torch.sqrt(torch.mean((R_re**2 + R_im**2), dim=0))
 
-            H_hat_mean = sigmoid(R_mean, self.S1, self.S2) 
+            H_hat_mean = sigmoid(R_mean, self.S1, self.S2, self.recived)
 
             H_hat_re = R_re * H_hat_mean
             H_hat_im = R_im * H_hat_mean
 
-            h_hat_re, h_hat_im = MM(H_hat_re, H_hat_im, self.DFT_re, self.DFT_im)
+            h_hat_re, h_hat_im = MM(
+                H_hat_re, H_hat_im, self.DFT_re, self.DFT_im)
 
             z_re = u_re - h_hat_re
             z_im = u_im - h_hat_im
@@ -59,50 +62,46 @@ class Net(torch.nn.Module):
     def __init__(self, cfg):
         super(Net, self).__init__()
 
-        self.network = nn.ModuleList([Net_Layer(cfg,48,512) for i in range(cfg.layers)])
+        self.network = nn.ModuleList(
+            [Net_Layer(cfg, 48, 512) for i in range(cfg.layers)])
 
         print("Created Net with ", cfg.layers, "layers")
 
-        self.DFT_re, self.DFT_im = DFT_matrixes(48,512)
-
+        self.DFT_re, self.DFT_im = DFT_matrixes(48, 512)
+        self.recived = cfg.recived
 
     def forward(self, u):
-        H_hat_re = torch.zeros([64,512])
-        H_hat_im = torch.zeros([64,512])
+        H_hat_re = torch.zeros([self.recived, 512])
+        H_hat_im = torch.zeros([self.recived, 512])
 
         u, maximum = self.normalize(u)
 
-        u_re = u[:,:,0]
-        u_im = u[:,:,1] 
+        u_re = u[:, :, 0]
+        u_im = u[:, :, 1]
 
         z_re = u_re
         z_im = u_im
 
         for layer in self.network:
-            z_re, z_im, H_hat_re, H_hat_im = layer(u_re, u_im, z_re, z_im, H_hat_re, H_hat_im)
-        
-        
-        h_hat_re, h_hat_im = MM(H_hat_re, H_hat_im, self.DFT_re, self.DFT_im) 
+            z_re, z_im, H_hat_re, H_hat_im = layer(
+                u_re, u_im, z_re, z_im, H_hat_re, H_hat_im)
 
-        out = torch.stack((h_hat_re, h_hat_im), dim = 2)
+        h_hat_re, h_hat_im = MM(H_hat_re, H_hat_im, self.DFT_re, self.DFT_im)
+
+        out = torch.stack((h_hat_re, h_hat_im), dim=2)
 
         out = self.denormalize(out, maximum)
         return out
-
-
 
     def normalize(self, u):
         maximum = abs(u.max())
         u = 5*u/maximum
         return u, maximum
 
-
-
     def denormalize(self, u, maximum):
         u = u/5*maximum
-        return u 
+        return u
 
- 
     def setState(self, trainable_code, pass_code):
 
         c = [bool(int(d)) for d in str(trainable_code)]
@@ -111,4 +110,3 @@ class Net(torch.nn.Module):
         for i, module in enumerate(self.network):
             module.setTrainable(c[i])
             module.isPass = p[i]
-
--- ch_est_net/activation.py
+++ ch_est_net/activation.py
@@ -1,43 +1,48 @@
 import torch
 
-def sigmoid(r, S1, S2):
+
+def sigmoid(r, S1, S2, recived):
     one = torch.tensor([1])
     res = one/(1+torch.exp(-S1*r+S2))
-    res = torch.reshape(res, (1,512))
-    res = res.expand((64,512))
+    res = torch.reshape(res, (1, 512))
+    res = res.expand((recived, 512))
     return res
+
 
 def arctan(r, S1, S2):
     pi = torch.tensor(np.pi)
     one_half = torch.tensor([0.5])
     res = torch.atan(S1*(r+S2))/pi + one_half
-    res = torch.reshape(res, (1,512))
-    res = res.expand((64,512))
-    
+    res = torch.reshape(res, (1, 512))
+    res = res.expand((64, 512))
+
     return res
 
-def tanh(r,S1,S2):
+
+def tanh(r, S1, S2):
     one_half = torch.tensor([0.5])
     res = one_half * torch.tanh(S1*(r+S2)) + one_half
-    res = torch.reshape(res, (1,512))
-    res = res.expand((64,512))
-    
+    res = torch.reshape(res, (1, 512))
+    res = res.expand((64, 512))
+
     return res
+
 
 def sqrt(r, S1, S2):
     one_half = torch.tensor([0.5])
     one = torch.tensor([1])
-    res = one_half * (S1*(r+S2)) / torch.sqrt(one+ (S1*(r+S2))**2 ) + one_half
-    res = torch.reshape(res, (1,512))
-    res = res.expand((64,512))
-    
+    res = one_half * (S1*(r+S2)) / torch.sqrt(one + (S1*(r+S2))**2) + one_half
+    res = torch.reshape(res, (1, 512))
+    res = res.expand((64, 512))
+
     return res
 
-def rat_sigmoid(r,S1,S2):
+
+def rat_sigmoid(r, S1, S2):
     one_half = torch.tensor([0.5])
     one = torch.tensor([1])
-    res = one_half* S1*(r+S2) / (abs(S1*(r+S2))+1) + one_half
-    res = torch.reshape(res, (1,512))
-    res = res.expand((64,512))
-    
+    res = one_half * S1*(r+S2) / (abs(S1*(r+S2))+1) + one_half
+    res = torch.reshape(res, (1, 512))
+    res = res.expand((64, 512))
+
     return res
